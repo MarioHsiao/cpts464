ffd988789000934b82b9e6593c86486188589318
Bryce Landon Handerson <bhanders@rti-dds-06.eecs.wsu.edu>
2014-01-17 10:37:21 -0800

    first project done

---
+++ proj1/littleSupport.h
@@ -0,0 +1,83 @@
+
+/*
+  WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+  This file was generated from little.idl using "rtiddsgen".
+  The rtiddsgen tool is part of the RTI Connext distribution.
+  For more information, type 'rtiddsgen -help' at a command shell
+  or consult the RTI Connext manual.
+*/
+
+#ifndef littleSupport_1487300790_h
+#define littleSupport_1487300790_h
+
+/* Uses */
+#include "little.h"
+
+
+
+#ifdef __cplusplus
+#ifndef ndds_cpp_h
+  #include "ndds/ndds_cpp.h"
+#endif
+#else
+#ifndef ndds_c_h
+  #include "ndds/ndds_c.h"
+#endif
+#endif
+
+        
+
+/* ========================================================================= */
+/**
+   Uses:     T
+
+   Defines:  TTypeSupport, TDataWriter, TDataReader
+
+   Organized using the well-documented "Generics Pattern" for
+   implementing generics in C and C++.
+*/
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+  /* If the code is building on Windows, start exporting symbols.
+   */
+  #undef NDDSUSERDllExport
+  #define NDDSUSERDllExport __declspec(dllexport)
+
+#ifdef __cplusplus
+  /* If we're building on Windows, explicitly import the superclasses of
+   * the types declared below.
+   */        
+  class __declspec(dllimport) DDSTypeSupport;
+  class __declspec(dllimport) DDSDataWriter;
+  class __declspec(dllimport) DDSDataReader;
+#endif
+
+#endif
+
+#ifdef __cplusplus
+
+DDS_TYPESUPPORT_CPP(LittleMsgTypeSupport, LittleMsg);
+
+DDS_DATAWRITER_CPP(LittleMsgDataWriter, LittleMsg);
+DDS_DATAREADER_CPP(LittleMsgDataReader, LittleMsgSeq, LittleMsg);
+
+
+#else
+
+DDS_TYPESUPPORT_C(LittleMsgTypeSupport, LittleMsg);
+DDS_DATAWRITER_C(LittleMsgDataWriter, LittleMsg);
+DDS_DATAREADER_C(LittleMsgDataReader, LittleMsgSeq, LittleMsg);
+
+#endif
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+  /* If the code is building on Windows, stop exporting symbols.
+   */
+  #undef NDDSUSERDllExport
+  #define NDDSUSERDllExport
+#endif
+
+
+
+#endif  /* littleSupport_1487300790_h */
